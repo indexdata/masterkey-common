86512fdadf64d7fdf7b47e3684267608484bd6d8
Dennis Schafroth <dennis@indexdata.com>
2011-09-26 11:14:09 +0200

    Isolate hard coded constants

---
+++ src/main/java/com/indexdata/masterkey/config/HardcodedModuleConfiguration.java
@@ -0,0 +1,48 @@
+package com.indexdata.masterkey.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+public class HardcodedModuleConfiguration implements  ModuleConfigurationGetter {
+
+  Logger logger = Logger.getLogger(this.getClass());
+  Map<String, String> hardcodedMap = new HashMap<String, String>();
+  ModuleConfiguration moduleConfig; 
+  
+  public HardcodedModuleConfiguration(ModuleConfiguration moduleConfig) {
+    this.moduleConfig = moduleConfig;
+  }
+
+  public void set(String name, String value) {
+    hardcodedMap.put(name, value);
+  }
+
+  public String get(String name) {
+    if (moduleConfig.get(name) != null)
+      return moduleConfig.get(name);
+    
+    if (hardcodedMap.containsKey(name)) {
+      logger.warn("Using hardcoded default for " + name + 
+	  ". Please update your plugin configuration for module " + moduleConfig.moduleName + 
+	  " with " + name +" = "+ hardcodedMap.get(name) + " to avoid this warning");
+      return hardcodedMap.get(name);
+    }
+    return null; 
+  }
+
+  public String get(String name, String defaultValue) {
+    if (moduleConfig.get(name) != null)
+      return moduleConfig.get(name);
+    
+    if (hardcodedMap.containsKey(name)) {
+      logger.warn("Using hardcoded default for " + name + 
+	  ". Please update your plugin configuration for module " + moduleConfig.moduleName + 
+	  " with " + name +" = "+ hardcodedMap.get(name) + " to avoid this warning");
+      return hardcodedMap.get(name);
+    }
+    return defaultValue; 
+  }
+
+}
