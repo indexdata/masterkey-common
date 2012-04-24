1afdf63b3b22c74c45452b036fa202e5d08898cd
Jakub Skoczen <jakub@indexdata.dk>
2012-04-24 11:56:41 +0200

    Not final

--- src/main/java/com/indexdata/masterkey/pazpar2/client/Pazpar2ClientConfiguration.java
+++ src/main/java/com/indexdata/masterkey/pazpar2/client/Pazpar2ClientConfiguration.java
@@ -25,7 +25,7 @@ public class Pazpar2ClientConfiguration {
   public String PAZPAR2_SERVICE_ID = null;
   public String PAZPAR2_SERVICE_XML = null;
   public String XML_FILE_PATH = null;
-  public final String USE_OPAQUE_ID;
+  public String USE_OPAQUE_ID;
   
   //map values
   public final String CCLMAP_TERM;
