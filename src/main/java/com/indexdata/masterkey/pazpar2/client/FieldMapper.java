d62c22ec2487bfd907ac10456d56433c0a741f5f
Jakub Skoczen <jakub@indexdata.dk>
2012-05-02 11:39:48 +0200

    Add directives parsing in field mapper
    E.g
    
      %ns ns_prefix http://somenamespace.com
    
    or
    
      %import base-stylesheet.xsl

--- src/main/java/com/indexdata/masterkey/pazpar2/client/FieldMapper.java
+++ src/main/java/com/indexdata/masterkey/pazpar2/client/FieldMapper.java
@@ -9,6 +9,10 @@ import com.indexdata.utils.XmlUtils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -20,10 +24,12 @@ import org.xml.sax.SAXException;
  * @author jakub
  */
 public class FieldMapper {
-  
+  public final static String NS_NS = "http://www.w3.org/2000/xmlns/";
   public final static String XSL_NS = "http://www.w3.org/1999/XSL/Transform";
   public final static String PZ2_NS = "http://www.indexdata.com/pazpar2/1.0";
   public final static String TMARC_NS = "http://www.indexdata.com/turbomarc";
+  
+  private final static String[] reservedPrefixes = {"xsl", "pz", "tmarc"};
   
   public enum MapType {
     MARC("base-tmarc"), XML("base-xml");
@@ -33,6 +39,22 @@ public class FieldMapper {
     }
     public String getBaseXSLName() {
       return baseXSLName;
+    }
+  }
+  
+  public enum Directive {
+    NAMESPACE("ns"), IMPORT("import");
+    private String name;
+    private Directive(String name) {
+      this.name = name;
+    }
+    public static Directive fromString(String name) {
+      for (Directive d : Directive.values()) {
+        if (d.name.equalsIgnoreCase(name)) {
+          return d;
+        }
+      }
+      return null;
     }
   }
   
@@ -95,33 +117,85 @@ public class FieldMapper {
   
   private ArrayList<FieldMap> fieldMaps;
   private MapType type;
+  private List<String> imports;
+  private Map<String,String> namespaces;
+
+  public List<String> getImports() {
+    return imports;
+  }
+
+  public ArrayList<FieldMap> getFieldMaps() {
+    return fieldMaps;
+  }
+
+  public Map<String, String> getNamespaces() {
+    return namespaces;
+  }
+  
+  private void parseNamespace(String ns, int lineNo) throws ParsingException {
+    int fws = ns.indexOf(" ");
+    if (fws < 1) 
+      throw new ParsingException("Malformed namespace directive: "+ns+" at line "+lineNo);
+    String name = ns.substring(0, fws).trim();
+    for (String v : reservedPrefixes) {
+      if (v.equals(name)) throw new ParsingException("Reserved prefix '"+name+"' cannot be redefined");
+    }
+    String value = ns.substring(fws).trim();
+    namespaces.put(name, value);
+  }
 
   public FieldMapper(String input) throws ParsingException {
-     String[] lines = input.split("\\n");
-     fieldMaps = new ArrayList<FieldMap>(lines.length);
-     for (int k = 0; k < lines.length; k++) {
-       //make sure to trim 
-       String line = lines[k].trim();
+    String[] lines = input.split("\\n");
+    fieldMaps = new ArrayList<FieldMap>(lines.length);
+    namespaces = new TreeMap<String, String>();
+    imports = new ArrayList<String>();
+    for (int k = 0; k < lines.length; k++) {
+      //make sure to trim 
+      String line = lines[k].trim();
+      if (line.isEmpty())
+        continue;
+      if (line.charAt(0) == '%') {
+        //lines starting with '%' are directives
+        String directive = line.substring(1).trim();
+        //%dir name some value
+        int fws = directive.indexOf(" ");
+        if (fws < 1)
+          throw new ParsingException("Malformed directive at line "+k+": "+line);
+        String dName = directive.substring(0, fws).trim();
+        String dValue = directive.substring(fws).trim();
+        Directive d = Directive.fromString(dName);
+        if (d == null)
+          throw new ParsingException("Unknown directive '"+dName+"' at line "+k+": "+line);
+        switch (d) {
+          case IMPORT: imports.add(dValue); break;
+          case NAMESPACE: parseNamespace(dValue, k); break;
+          default: throw new 
+            ParsingException("Unsupported directive '"+dName+"' at line "+k+": "+line);
+        }
+      } else {
         //xpath can contain whitespace but element name do not
         int lws = line.lastIndexOf(" ");
-        if (lws < 1) throw new ParsingException("Delimiter at wrong position ("
-          +lws+"): "+line);
+        if (lws < 1)
+          throw new ParsingException("Delimiter at wrong position ("
+            + lws + "): " + line);
         String selector = line.substring(0, lws).trim();
         String name = line.substring(lws).trim();
         //xpath cannot start with a digit
-        MapType lineType = Character.isDigit(selector.charAt(0)) ? MapType.MARC : MapType.XML;
+        MapType lineType = Character.isDigit(selector.charAt(0)) ? MapType.MARC
+          : MapType.XML;
         if (type == null) {
           type = lineType;
         } else if (type != lineType) {
           throw new ParsingException("Unexpected line of type '"
-            +lineType+"' in the fieldMap type "+type+": "+line);
+            + lineType + "' in the fieldMap type " + type + ": " + line);
         }
         if (type == MapType.XML) {
           fieldMaps.add(new FieldMap(selector, name));
         } else if (type == MapType.MARC) {
           String[] pair = selector.split("\\$");
-          if (pair.length != 2) throw new 
-            ParsingException("Malformed MARC selector ("+selector+"): "+line);
+          if (pair.length != 2)
+            throw new ParsingException("Malformed MARC selector (" + selector
+              + "): " + line);
           String datafield = pair[0];
           String subfields = pair[1];
           boolean inc = true;
@@ -130,10 +204,12 @@ public class FieldMapper {
             subfields = subfields.substring(1);
             inc = false;
           }
-          if (subfields.equals("*")) subfields = "";
+          if (subfields.equals("*"))
+            subfields = "";
           fieldMaps.add(new FieldMap(datafield, name, subfields, inc));
         }
-      }  
+      }
+    }
   }
   
   public Document getStylesheet() throws ParsingException {
@@ -161,13 +237,8 @@ public class FieldMapper {
     } else {
       ss = XmlUtils.newDoc();
       Element root = ss.createElementNS(XSL_NS, "xsl:stylesheet");
-      root.setAttributeNS("http://www.w3.org/2000/xmlns/",
-        "xmlns:xsl",
-        XSL_NS);
-      root.setAttributeNS("http://www.w3.org/2000/xmlns/",
-        "xmlns:pz",
-        PZ2_NS);
-      root.setAttribute("version", "1.0");
+      root.setAttributeNS(NS_NS, "xmlns:xsl", XSL_NS);
+      root.setAttributeNS(NS_NS, "xmlns:pz", PZ2_NS);
       ss.appendChild(root);
       //output indent="yes" method="xml" version="1.0" encoding="UTF-8"
       Element out = ss.createElementNS(XSL_NS, "xsl:output");
@@ -183,6 +254,18 @@ public class FieldMapper {
       Element txtTmpl = ss.createElementNS(XSL_NS, "xsl:template");
       txtTmpl.setAttribute("match", "text()");
       root.appendChild(txtTmpl);
+    }
+    //namespaces
+    Element root = ss.getDocumentElement();
+    for (Entry<String, String> e : namespaces.entrySet()) {
+      root.setAttributeNS(NS_NS, "xmlns:" + e.getKey(), e.getValue());
+    }
+    //imports
+    Node firstChild = root.getFirstChild();
+    for (String href : imports) {
+      Element impE = ss.createElementNS(XSL_NS, "xsl:import");
+      impE.setAttribute("href", href);
+      root.insertBefore(impE, firstChild);
     }
     switch (type) {
       case MARC: return getMarcStylesheet(tmpl, pzRecord);
