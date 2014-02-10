a583914187a23996182f7e148796b124e2951eac
Dennis Schafroth <dennis@indexdata.com>
2014-02-10 08:19:46 +0100

    Implement Dynamic Element helper methods

--- src/main/java/com/indexdata/torus/Layer.java
+++ src/main/java/com/indexdata/torus/Layer.java
@@ -6,10 +6,15 @@
 
 package com.indexdata.torus;
 
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
+
 import javax.xml.bind.annotation.XmlAnyElement;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
+
+import com.indexdata.torus.layer.DynamicElement;
 
 /**
  * Represents a single abstract layer within a record.
@@ -20,6 +25,7 @@ public abstract class Layer {
     private String id;
     private String layerName;
     private List<Object> otherElements;
+    private List<DynamicElement> elements;
         
     public Layer() {
     }
@@ -33,7 +39,10 @@ public abstract class Layer {
         return layerName;
     }
 
-    @Deprecated 
+    /**
+     *  use setLayerName 
+     */
+    @Deprecated
     final public void seLayertName(String name) {
     	setLayerName(name);
     }
@@ -55,12 +64,65 @@ public abstract class Layer {
      * of this class.
      * @return a list of Elements or JAXBElements objects
      */
-    @XmlAnyElement
+    @Deprecated
     public List<Object> getOtherElements() {
         return otherElements;
     }
 
+    /**
+     * Use DynamicsElements or set/add Element
+     * @param otherElements
+     */
+    @Deprecated
     public void setOtherElements(List<Object> otherElements) {
         this.otherElements = otherElements;
+    }
+
+    @XmlAnyElement
+    public Collection<DynamicElement> getDynamicElements() {
+        return elements;
+    }
+
+    public void setDynamicElements(List<DynamicElement> elements) {
+        this.elements = elements;
     }    
+
+    public void setElement(String key, Object value) {
+      if (value == null)
+	return ; 
+      if (elements == null)
+	elements = new LinkedList<DynamicElement>();
+      for (DynamicElement element : elements) {
+	if (key.equals(element.getName())) { 
+	  element.setValue(value);
+	  return ;
+	}
+      }
+      elements.add(new DynamicElement(key, value));
+    }
+    
+    public void addElement(String key, Object value) {
+      if (value == null)
+	return ; 
+      if (elements == null)
+	elements = new LinkedList<DynamicElement>();
+      elements.add(new DynamicElement(key, value));
+    }
+
+    public Collection<DynamicElement> getElement(String key) {
+      Collection<DynamicElement> subset = new LinkedList<DynamicElement>();
+      if (elements == null)
+	return subset; 
+      for (DynamicElement element : elements) {
+	if (key.equals(element.getName())) { 
+	  subset.add(element);
+	}
+      }
+      return subset;
+    }
+
+    public void deleteElement(String key) {
+      Collection<DynamicElement> deletes = getElement(key);
+      elements.removeAll(deletes);
+    }
 }
